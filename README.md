f2a08dde72c1168e98624e16e64b12f8a4f2071b
UrsusSalificus <titouan.laessle@unil.ch>
2017-04-12 17:22:09 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,13 @@
+## SAGE course 2017: Core phylogeny
+Sequence A GEnome (SAGE) course  is taking place Lausanne University (Swiss) and involve sequencing and analyzing a bacterial genome.
+This year course focuses on the bumble and honey bees gut-microbiota, more precisely on Firm-5 strain.
+This Repo contains all the script used to perform the core phylogeny on the different strains found.  
+
+Brief explanation of files found in this repo:
+
+```
+paralogs_only.py  :  will extract paralogs, from a list of homologous genes.
+to_have_test_homolog.sh : simple reminder of how to create a test file from bigger file
+
+```
+
---
+++ paralogs_only.py
@@ -0,0 +1,58 @@
+# To extract paralogs from a list of homologs:
+
+import glob
+#import sys
+
+# Path to the directory containing all the genomes:
+genomes_directory = "../../genome_files/"
+# Path to the input file (here the list of all homologs)
+oldfile = "../files/mclOutput"
+# Path to the output file (here the list of all the paralogs only)
+newfile = "../files/orthologs_only"
+
+###
+# Extract the genomes used when looking for homologs
+###
+def extract_genomes(path):
+    return (glob.glob(path + "*.faa"))
+
+genomes = extract_genomes(path = genomes_directory)
+
+
+
+###
+# Filter homolog list into list of paralogs only
+# Read a list of families, then keep if the number of genes in the family
+# equal the number of different genomes used to construct the homolog table,
+# then if it is equal, check if there is no dupplicate.
+# Keep all the families that pass both requirements family of paralogs only.
+# inputs:
+#   oldfile: Path to the input file (here the list of all homologs)
+#   newfile: Path to the output file (here the list of all the paralogs only)
+#   genomes: Genomes used to create the homolog list
+###
+def filter(oldfile, newfile, genomes):
+    # First open the output file in writing mode as 'outfile',
+    # Then open the input file in reading mode as 'infile'
+    with open(newfile, 'w') as outfile, open(oldfile, 'r') as infile:
+        # For each line (=each homolog family), split to have each members separated
+        # (split() does the job as they are separated by \t)
+        for line in infile:
+            splitted = line.split()
+            # Check if the number of members in the family equal the number of genomes
+            # used to create the homolog list
+            if len(splitted) == len(genomes):
+                # Check if there is no genome duplicate (indicates orthologs):
+                all_genomes = []  # list initialization
+                # For each members (each look like 'genome_code'|'gene_number'):
+                # split them to keep only the genome code, and store each in a list
+                for each_gene in splitted:
+                    all_genomes.append(each_gene.split('|')[0])
+                # Now check if the each genome code appears only once
+                # by comparing list of unique (set()) elements number
+                # against the number of genomes used to create the homolog list
+                if len(set(all_genomes)) == len(genomes):
+                    # If does, write the whole line (=family) in the output file
+                    outfile.write(line)
+
+filter(oldfile = oldfile, newfile = newfile, genomes = genomes)
